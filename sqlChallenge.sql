d11185b75db922ce091266e66d6eaf60824b9433
Matyas Chlebovsky <Matyass@users.noreply.github.com>
2018-03-27 13:31:14 -0400

    third and fourth query completed

--- sqlChallenge.sql
+++ sqlChallenge.sql
@@ -58,3 +58,26 @@ where hit_timestamp > DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 7 day);
 select count(session_id)
 from ga_session_features
 where medium = 'organic';
+
+-- Query 3
+-- all nodes and their pageviews with organic medium in the last 30 days
+select node_id, sum(pageviews)
+from ga_session_views views
+inner join ga_session_features features
+  on features.session_id = views.session_id
+where medium = 'organic' and hit_timestamp > DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 30 day)
+group by node_id
+order by pageviews desc;
+
+-- Query 4
+-- An average session length
+select device_category, avg(ga.sessionLen) as avgSessionLength
+from ga_session_features features
+inner join
+  (
+  select session_id, timestampdiff(minute, min(hit_timestamp), max(hit_timestamp)) as sessionLen
+  from ga_session_views
+  group by session_id
+  ) ga
+    on features.session_id = ga.session_id
+group by device_category;

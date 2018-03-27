c6e8157911ad00100c80d2882557d555789d6b48
Matyas Chlebovsky <Matyass@users.noreply.github.com>
2018-03-27 13:41:28 -0400

    minor changes in comments in sqlChallenge

--- sqlChallenge.sql
+++ sqlChallenge.sql
@@ -50,17 +50,19 @@ insert into eotssChallenge.ga_session_features values
 (2005, 'direct', 'mobile', 'Safari');
 
 -- Query 1
+-- total pageviews in last 7 days
 select sum(pageviews)
 from ga_session_views
 where hit_timestamp > DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 7 day);
 
 -- Query 2
+-- all sessions with organic medium
 select count(session_id)
 from ga_session_features
 where medium = 'organic';
 
 -- Query 3
--- all nodes and their pageviews with organic medium in the last 30 days
+-- nodes and their pageviews with organic medium in the last 30 days
 select node_id, sum(pageviews)
 from ga_session_views views
 inner join ga_session_features features
@@ -70,14 +72,15 @@ group by node_id
 order by pageviews desc;
 
 -- Query 4
--- An average session length
-select device_category, avg(ga.sessionLen) as avgSessionLength
+-- This query calculates each session's duration in minutes and then averages the duration by category
+select device_category, avg(t1.sessionLen) as avgSessionLength
 from ga_session_features features
 inner join
   (
+  -- calculate the duration of each session
   select session_id, timestampdiff(minute, min(hit_timestamp), max(hit_timestamp)) as sessionLen
   from ga_session_views
   group by session_id
-  ) ga
-    on features.session_id = ga.session_id
+  ) t1
+    on features.session_id = t1.session_id
 group by device_category;
